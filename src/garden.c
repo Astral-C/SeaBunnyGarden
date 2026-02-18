a3f1aaf7a2c40d1a9f5fd4cad5bfce229ed290ab
d3thcoff13 <coffeeisdeath@gmail.com>
2026-02-17 21:31:00 -0500

    added simple menu to garden

--- src/garden.c
+++ src/garden.c
@@ -6,20 +6,50 @@
 
 static menu garden_menu;
 
+static menu_item* cursor;
+
+static menu_item* bun1;
+static menu_item* bun2;
 
 void garden_enter(gamestate* state) {
-	menu_init(&garden_menu, 3);
+	menu_init(&garden_menu, 6);
 
-	//menu_add_text(&garden_menu, "Test Text", 50, 50);
-	menu_add_text(&garden_menu, "dfasfdaf", 50, 50);
-	menu_item* frame = menu_add_rect_outline(&garden_menu, 12, 12, 290, 210, 4);
-	frame->color = RGBA32(0xff, 0x33, 0x33, 0xff);
 
-	//menu_add_text(&garden_menu, "Menu", 50, 50);
-	//menu_add_text(&garden_menu, "Buns", 50, 20);
+	cursor = menu_add_rect_outline(&garden_menu, 0, 0, 38, 38, 4);
+	cursor->color = RGBA32(0xff, 0xff, 0x33, 0xff);
+
+	bun1 = menu_add_image(&garden_menu, "rom:/bun.sprite");
+	bun1->x = display_get_width() / 4;
+	bun1->y = display_get_height() / 3 + 55;
+	bun1->sx = 1;
+	bun1->sy = 1;
+
+	bun2 = menu_add_image(&garden_menu, "rom:/bun.sprite");
+	bun2->x = display_get_width() * 3 / 4;
+	bun2->y = display_get_height() / 3 + 55;
+	bun2->sx = 1;
+	bun2->sy = 1;
+
+	menu_add_text(&garden_menu, "Menu", display_get_width() / 2, 50);
+	menu_add_text(&garden_menu, "Bun1", bun1->x, bun1->y -30);
+	menu_add_text(&garden_menu, "Bun2", bun2->x -1 , bun2->y -30);
+
+	cursor->x = bun1->x - 2;
+	cursor->y = bun1->y - 2;
+
 }
 
-void garden_update(gamestate* state){}
+void garden_update(gamestate* state){
+	joypad_inputs_t joypad = joypad_get_inputs(JOYPAD_PORT_1);
+	//if (joypad.btn.d_left) {
+	if(joypad.stick_x < 0){
+		if(bun1)cursor->x = bun1->x - 2;
+	}
+	//if (joypad.btn.d_right) {
+	if (joypad.stick_x > 0){
+		if(bun2)cursor->x = bun2->x -2;
+	}
+}
 void garden_exit(gamestate* state){}
 void garden_draw_2d(gamestate* state){
 	menu_draw(&garden_menu);
